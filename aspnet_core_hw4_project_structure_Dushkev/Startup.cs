0d2774f892ced03eac1db11209380c0e3387365b
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2018-07-13 12:12:18 -0400

    AutoMapper mapping added

--- aspnet_core_hw4_project_structure_Dushkev/Startup.cs
+++ aspnet_core_hw4_project_structure_Dushkev/Startup.cs
@@ -8,6 +8,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using AutoMapper;
+using DAL.Model;
+using Shared.DTOs;
+
 
 namespace aspnet_core_hw4_project_structure_Dushkev
 {
@@ -24,6 +28,8 @@ namespace aspnet_core_hw4_project_structure_Dushkev
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            var mapper = MapperConfiguration().CreateMapper();
+            services.AddScoped(_ => mapper);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,37 @@ namespace aspnet_core_hw4_project_structure_Dushkev
             }
 
             app.UseMvc();
+        }
+
+        public MapperConfiguration MapperConfiguration()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<PlaneType, PlaneTypeDTO>();
+                cfg.CreateMap<PlaneTypeDTO, PlaneType>();
+
+                cfg.CreateMap<Plane, PlaneDTO>();
+                cfg.CreateMap<PlaneDTO, Plane>();
+
+                cfg.CreateMap<Stewardess, StewardessDTO>();
+                cfg.CreateMap<StewardessDTO, Stewardess>();
+
+                cfg.CreateMap<Pilot, PilotDTO>();
+                cfg.CreateMap<PilotDTO, Pilot>();
+
+                cfg.CreateMap<Crew, CrewDTO>();
+                cfg.CreateMap<CrewDTO, Crew>();
+
+                cfg.CreateMap<Ticket, TicketDTO>();
+                cfg.CreateMap<TicketDTO, Ticket>();
+
+                cfg.CreateMap<Flight, FlightDTO>();
+                cfg.CreateMap<FlightDTO, Flight>();
+
+                cfg.CreateMap<Departure, DepartureDTO>();
+                cfg.CreateMap<DepartureDTO, Departure>();
+            });
+            return config;
         }
     }
 }
